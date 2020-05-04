e438ccbb9530f3ca4460f657baacaec89f2c0374
Diogo Andrade <diogo.g.andrade@gmail.com>
2020-05-04 13:15:40 +0100

    Working rope test

---
+++ Assets/Scripts/RopeDJ.cs
@@ -0,0 +1,240 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RopeDJ : MonoBehaviour
+{
+    public Rigidbody2D  connectedObject;
+    public LayerMask    collisionMask;
+    public float        collisionTolerance = 1;
+
+
+    class Joint
+    {
+        public DistanceJoint2D  dj;
+        public FixedJoint2D     fj;
+    }
+
+    List<Joint>     joints;
+    LineRenderer    lineRenderer;
+
+    private void Awake()
+    {
+        var children = GetComponentsInChildren<Transform>();
+        foreach (var child in children)
+        {
+            if (child != transform)
+            {
+#if UNITY_EDITOR
+                if (UnityEditor.EditorApplication.isPlaying)
+                    Destroy(child.gameObject);
+                else
+                    DestroyImmediate(child.gameObject);
+#else
+                Destroy(child.gameObject);
+#endif
+            }
+        }
+    }
+
+    void Start()
+    {
+        joints = new List<Joint>();
+
+        CreateNewJoint(transform.position);
+
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+#if UNITY_EDITOR
+        if (!UnityEditor.EditorApplication.isPlaying) return;
+        if (UnityEditor.EditorApplication.isCompiling) return;
+#endif
+        CreateNewLink();
+    }
+
+    private void Update()
+    {
+        if (!lineRenderer)
+        {
+            lineRenderer = GetComponent<LineRenderer>();
+        }
+
+        if (lineRenderer)
+        {
+            if ((joints == null) || (joints.Count == 0))
+            {
+                if (connectedObject)
+                {
+                    lineRenderer.positionCount = 2;
+                    lineRenderer.SetPosition(0, transform.position);
+                    lineRenderer.SetPosition(1, connectedObject.transform.position);
+                }
+            }
+            else
+            {
+#if UNITY_EDITOR
+                joints.RemoveAll((x) => (x.dj == null));
+#endif
+
+                lineRenderer.positionCount = joints.Count + 1;
+
+                lineRenderer.SetPosition(0, transform.position);
+
+                for (int i = 0; i < joints.Count; i++)
+                {
+                    lineRenderer.SetPosition(i + 1, joints[i].dj.connectedBody.position);
+                }
+            }
+        }
+    }
+
+    void CreateNewLink()
+    {
+        Joint j = joints[joints.Count - 1];
+
+        // See if we have to break the segment 
+        Vector2 startPos = j.dj.transform.position;
+        Vector2 endPos = j.dj.connectedBody.position;
+
+        var hits = GetHits(startPos, endPos);
+
+        foreach (var hit in hits)
+        {
+            Vector2 hookPos = GetHookPosition(j.dj.transform.position);
+
+            j.dj.enabled = false;
+            j.fj = j.dj.gameObject.AddComponent<FixedJoint2D>();
+            j.fj.connectedBody = connectedObject;
+
+            var newRB = CreateNewJoint(hookPos);
+            j.dj.connectedBody = newRB;
+            j.fj.connectedBody = newRB;
+
+            return;
+        }
+
+        if (joints.Count > 1)
+        {
+            // See if we can remove this link
+            Vector3 prevLinkPos = joints[joints.Count - 2].dj.transform.position;
+            Vector3 currentLinkPos = joints[joints.Count - 1].dj.transform.position;
+            Vector3 objectPos = connectedObject.transform.position;
+            // Check if we can "see" the previous link start
+            startPos = prevLinkPos;
+
+            hits = GetHits(startPos, objectPos);
+
+            if (hits.Count > 0) return;
+
+            // Check for colinearity between this link and the previous
+            Vector2 dir1 = currentLinkPos - prevLinkPos;
+            Vector2 dir2 = objectPos - currentLinkPos;
+
+            if (Vector2.Dot(dir1.normalized, dir2.normalized) < 0.9f) return;
+
+            /*            endPos = joints[joints.Count - 1].dj.transform.position;
+
+                        hits = GetHits(startPos, endPos);
+
+                        if (hits.Count > 0) return;*/
+
+            // We can destroy this segment
+            Destroy(j.dj.gameObject);
+            joints.Remove(j);
+
+            // Reactivate previous link
+            j = joints[joints.Count - 1];
+
+            Destroy(j.fj); j.fj = null;
+            j.dj.enabled = true;
+            j.dj.connectedBody = connectedObject;
+        }
+    }
+
+    Vector3 GetHookPosition(Vector2 startPos)
+    {
+        // Find position, raycast from object to position
+        Vector2 dir = startPos - connectedObject.position;
+        float distance = dir.magnitude;
+        var hits = Physics2D.RaycastAll(connectedObject.position, dir, distance, collisionMask);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider.gameObject != connectedObject.gameObject)
+            {
+                return hit.point;
+            }
+        }
+
+        return startPos;
+    }
+
+    List<RaycastHit2D> GetHits(Vector2 startPos, Vector2 endPos)
+    {
+        List<RaycastHit2D>  ret = new List<RaycastHit2D>();
+        Vector2             dir = endPos - startPos;
+        float               distance = dir.magnitude;
+        var                 hits = Physics2D.RaycastAll(startPos + dir.normalized * collisionTolerance, dir, distance - 2 * collisionTolerance, collisionMask);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider.gameObject != connectedObject.gameObject)
+            {
+                ret.Add(hit);
+            }
+        }
+
+        return ret;
+    }
+
+    Rigidbody2D CreateNewJoint(Vector2 position)
+    {
+        // Create new joint
+        Joint newJoint = new Joint();
+
+        var go = new GameObject();
+        go.name = "RopeElem";
+        go.transform.parent = transform;
+        go.transform.position = position;
+        newJoint.dj = go.AddComponent<DistanceJoint2D>();
+        newJoint.dj.connectedBody = connectedObject;
+        newJoint.fj = null;
+        var rb = go.GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            rb = go.AddComponent<Rigidbody2D>();
+        }
+        rb.bodyType = RigidbodyType2D.Static;
+
+        joints.Add(newJoint);
+
+        return rb;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (joints != null)
+        {
+            for (int i = 0; i < joints.Count; i++)
+            {
+                var j = joints[i];
+                if (i == joints.Count - 1) Gizmos.color = Color.green;
+                else Gizmos.color = Color.yellow;
+
+                Gizmos.DrawLine(j.dj.transform.position, j.dj.connectedBody.transform.position);
+            }
+        }
+        else
+        {
+            if (connectedObject)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(transform.position, connectedObject.transform.position);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# Rope Test
+
+This is a test I've created to try out some rope physics on Unity.
+
+Ended up doing a more or less generic component that used DistanceJoint2d and FixedJoint2d to achieve the rope effect.
+
+## Tech stuff
+
+The Rope component just needs to be added to the "anchor point" of the rope, and the connectedObject property set to the object that we want to tie to the object.
+Then, the component creates a distance joint 2d (with default parameters, some adjustments would have to be done on this if you want to fix the distance, or use a sprint joint, etc) and uses that for the swinging motion using regular Unity physics. Then, a ray is cast into the scene between the anchor point and the object, if it collides with something, the distance joint is replaced by a fixed joint, and a new object is created with a distance joint between the intersection point and the object, and so forth.
+
+This is reverted when there is "line of sight" between the previous joint and the connected object, accounting for co-linearity between the previous segment and the current one.
+
+The code ended up not being too complex, but it's quite cool, even in the current form.
+
+## Credits
+
+* Code [Diogo Andrade]
+* Spike ball art by [Julien]
+* Chain element by [Fun Punch Games]
+
+## Licenses
+
+All code in this repo is made available through the [GPLv3] license.
+
+Spike ball: © 2005-2013 Julien Jorge <julien.jorge@stuff-o-matic.com>
+Chain Element: © 2017 Fun Punch Games Ltd. 
+
+## Metadata
+
+* Autor: [Diogo Andrade][]
+
+[Diogo Andrade]:https://github.com/DiogoDeAndrade
+[Fun Punch Games]:https://funpunchgames.com/
+[Julien]:https://opengameart.org/content/spike-ball
+[GPLv3]:https://www.gnu.org/licenses/gpl-3.0.en.html
+[CC-BY-SA 3.0.]:http://creativecommons.org/licenses/by-sa/3.0/
+[CC BY-NC-SA 4.0]:https://creativecommons.org/licenses/by-nc-sa/4.0/
